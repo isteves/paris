d4b3ba797972dbf2c6669f959d0517f78ac5263d
Irene <25118334+isteves@users.noreply.github.com>
2018-08-31 23:14:25 +0200

    add comments

--- scripts/ex_encoding-seprows.R
+++ scripts/ex_encoding-seprows.R
@@ -6,18 +6,23 @@ lunch <- tribble(
     "Tammy", "salad; crêpe"
 )
 
+# Encoding is handled differently by base R vs the tidyverse
 write.csv(lunch, "lunch.csv", row.names = FALSE)
 read.csv("lunch.csv")
 write_csv(lunch, "lunch.csv")
 read_csv("lunch.csv")
 
+# To separate all lunch_items by semicolons, you can use a `str_split` and `unnest`
 lunch %>% 
     mutate(lunch_items = str_split(lunch_items, "; ")) %>% 
     unnest(lunch_items)
 
+# ...but the `separate_rows` function does the same thing even more concisely!
 lunch %>% 
-    separate_rows(lunch_items)
+    separate_rows(lunch_items, sep = "; ")
 
+# lunch_items is essentially a kind of list column
+# You can turn it into a real list-column by using either `nest` or `list`. Notice how the two methods produce slightly different types of list-columns
 lunch %>% 
     separate_rows(lunch_items) %>% 
     nest(lunch_items) 
--- scripts/ex_periods.R
+++ scripts/ex_periods.R
@@ -7,26 +7,33 @@ events <- tibble(event = c("restaurant week", "shopalooza",
                  start = ymd("2018-08-30") + sample(5, 5),
                  end = start + sample(5, 5))
 
+# When do the most events overlap?
+
+# Plot to visually check the overlap
 events %>% 
     gather(bounds, dates, -event) %>% 
     ggplot(aes(dates, event)) +
     geom_point() +
     geom_line()
 
+# For each event, list out all dates
 all <- events %>% 
     mutate(period = end - start) %>% 
     mutate(all_dates = map2(start, period, ~.x + 0:.y)) %>% 
     unnest(all_dates) 
 
+# Plot to check the changes
 all %>% 
     ggplot(aes(all_dates, event)) +
     geom_point() +
     geom_line()
 
+# Visualize number of events for each date
 all %>% 
     ggplot(aes(all_dates)) +
     geom_bar()
 
+# Or find the number of events for each date analytically
 all %>% 
     count(all_dates) %>% 
     top_n(1)
